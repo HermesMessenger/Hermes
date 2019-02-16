b86197d92dc58860a485ef7085c83816d7b3ba1b
theperkinrex <theperkinrex@gmail.com>
2019-02-16 17:56:22 +0100

    Upgrade prepareProject.js

--- scripts/prepareProject.js
+++ scripts/prepareProject.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const fileExists = require('file-exists');
 const TimeUUID = require('cassandra-driver').types.TimeUuid;
-const configTemplate = 
-`{
-    "generalToken": "${new TimeUUID()}",
-    "mainIP": "localhost:8080",
-    "port": 8080
-}`;
+const configTemplate = {
+    generalToken: `${new TimeUUID()}`,
+    mainIP: "localhost:8080",
+    port: 8080
+};
 
-function writeJSON(){
-    fs.writeFile('config.json', configTemplate, function (err) {
+function writeJSON(json = {}){
+    let config = configTemplate;
+    for(key of Object.keys(config)){
+        if(json[key]){
+            config[key] = json[key]
+        }
+    }
+    fs.writeFile('config.json', JSON.stringify(config, null, 4), function (err) {
         if (err) throw err;
         console.log('config.json created');
     });
@@ -19,10 +24,17 @@ fileExists('config.json',(err,exists) => {
     if(err) throw err;
     if(exists) {
         const config = require('../config.json');
-        if(!config.port || !config.mainIP || !config.generalToken){
-            writeJSON();
+        let config_correct = true;
+        for(key of Object.keys(configTemplate)){
+            if(!config[key]){
+                config_correct = false;
+            }
+        }
+        if(!config_correct){
+            console.log('Something\'s missing in config.json, recreating it')
+            writeJSON(config);
         }else{
-            console.log('config.json correct');
+            console.log('config.json is correct');
         }
     }else{
         writeJSON();
