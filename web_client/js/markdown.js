5725c92c2daf795085e1bb72c4bd62caab873371
theperkinrex <theperkinrex@gmail.com>
2019-02-19 22:45:54 +0100

    Start the other direction parser

--- web_client/js/markdown.js
+++ web_client/js/markdown.js
@@ -1,10 +1,20 @@
 // Anything within the tag with $nº will be replaced with that group
 const MD_RULES = [
-    { regex: /(?:[^*]|^)(\*\*(.+?)\*\*)(?:[^*]|$)/, text_group: 1, tag: '<a class="MD-bold">', replace_group: 0 }, // This one has to go first beacause it overides the latter
-    { regex: /\*(.+?)\*/, text_group: 0, tag: '<a class="MD-italics">' },
-    { regex: /~(.+?)~/, text_group: 0, tag: '<a class="MD-strike">' },
-    { regex: /\[(.+?)\]\(((http:\/\/|https:\/\/).+?)\)/, text_group: 0, tag: '<a class="MD-link" href="$1">' },
+    { regex: /(?:[^*]|^)(\*\*(.+?)\*\*)(?:[^*]|$)/, text_group: 1, tag: '<span class="MD-bold">', replace_group: 0 }, // This one has to go first beacause it overides the latter
+    { regex: /\*(.+?)\*/, text_group: 0, tag: '<span class="MD-italics">' },
+    { regex: /~(.+?)~/, text_group: 0, tag: '<span class="MD-strike">' },
+    { regex: /\[(.+?)\]\(((http:\/\/|https:\/\/).+?)\)/, text_group: 0, tag: '<span class="MD-link" href="$1">' },
 ]
+
+/**
+ * @param {String} HTML representing any number of sibling elements
+ * @return {NodeList} 
+ */
+function htmlToElements(html) {
+    var template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content.childNodes;
+}
 
 
 function MDtoHTML(MD_String) {
@@ -18,16 +28,16 @@ function MDtoHTML(MD_String) {
             for (let m_str of all_matching_strings) {
                 let match = m_str.match(current_regex_not_global);
                 let current_tag = current_rule.tag;
-                if(dollar_ms = current_rule.tag.match(/\$(\d+)/g)){
-                    for (let dollar_s of dollar_ms){
-                        let dollar_m = dollar_s.replace('$','');
+                if (dollar_ms = current_rule.tag.match(/\$(\d+)/g)) {
+                    for (let dollar_s of dollar_ms) {
+                        let dollar_m = dollar_s.replace('$', '');
                         let group_number = parseInt(dollar_m);
-                        current_tag = current_tag.replace(dollar_s, match[group_number+1])
+                        current_tag = current_tag.replace(dollar_s, match[group_number + 1])
                     }
                 }
                 let element = $(current_tag).html/* TODO make this XSS invulnerable */(match[current_rule.text_group + 1])
                 let to_replace = match[0];
-                if(current_rule.replace_group != undefined) to_replace = match[current_rule.replace_group + 1]
+                if (current_rule.replace_group != undefined) to_replace = match[current_rule.replace_group + 1]
                 r = r.replace(to_replace, element.prop('outerHTML'));
             }
         }
@@ -35,6 +45,7 @@ function MDtoHTML(MD_String) {
     return r;
 }
 
-function HTMLtoMD(Html) {
-
+function HTMLtoMD(html) {
+    r = htmlToElements(html);
+    console.log(r);
 }
