2d8b58915e4f1c008d7f272aeffc4ae99144387c
theperkinrex <theperkinrex@gmail.com>
2019-02-20 19:20:06 +0100

    finish both-way implementation of the parser

--- web_client/js/markdown.js
+++ web_client/js/markdown.js
@@ -1,9 +1,18 @@
+// ### RULES FOR THE MD -> HTML PARSER ### //
 // Anything within the tag with $nº will be replaced with that group
 const MD_RULES = [
     { regex: /(?:[^*]|^)(\*\*(.+?)\*\*)(?:[^*]|$)/, text_group: 1, tag: '<b class="MD-bold">', replace_group: 0 }, // This one has to go first beacause it overides the latter
     { regex: /\*(.+?)\*/, text_group: 0, tag: '<i class="MD-italics">' },
     { regex: /~(.+?)~/, text_group: 0, tag: '<strike class="MD-strike">' },
     { regex: /\[(.+?)\]\(((http:\/\/|https:\/\/).+?)\)/, text_group: 0, tag: '<span style="display: inline;" class="MD-link" href="$1">' },
+]
+
+// ### RULES FOR THE HTML -> MD PARSER ### //
+const HTML_RULES = [
+    { class: 'MD-bold', md: '**$TEXT**' },
+    { class: 'MD-italics', md: '*$TEXT*' },
+    { class: 'MD-strike', md: '~$TEXT~' },
+    { class: 'MD-link', md: '[$TEXT]($HREF)' },
 ]
 
 /**
@@ -21,9 +30,9 @@ function MDtoHTML(MD_String) {
     let r = MD_String;
     for (let current_rule of MD_RULES) {
         let current_regex = current_rule.regex;
-        let current_regex_global = new RegExp(current_regex.source, 'g');
+        let current_regex_global = new RegExp(current_regex.source, 'g'); // Set to global to find all matches
         let all_matching_strings = r.match(current_regex_global);
-        let current_regex_not_global = new RegExp(current_regex.source, '');
+        let current_regex_not_global = new RegExp(current_regex.source, ''); // Set to not global to find the groups in each match
         if (all_matching_strings) {
             for (let m_str of all_matching_strings) {
                 let match = m_str.match(current_regex_not_global);
@@ -46,6 +55,32 @@ function MDtoHTML(MD_String) {
 }
 
 function HTMLtoMD(html) {
-    r = htmlToElements(html);
-    console.log(r);
+    let nodes = htmlToElements(html);
+    let md = '';
+    for (node of nodes) {
+        let innerMD = ''
+        if (node.childNodes.length > 0) {
+            for (let rule of HTML_RULES){
+                if(node.classList.contains(rule.class)){
+                    let replaceVal = rule.md;
+                    let href = node.getAttribute('href');
+                    let inner_html = node.innerHTML;
+                    let html_to_md = HTMLtoMD(inner_html);
+                    replaceVal = replaceVal.replace('$TEXT', html_to_md);
+                    if(href){
+                        replaceVal = replaceVal.replace('$HREF',href);
+                    }
+                    innerMD = replaceVal;
+                    break;
+                }
+            }
+        } else {
+            if (node.nodeValue != null) {
+                innerMD = node.nodeValue;
+            }
+        }
+
+        md += innerMD;
+    }
+    return md;
 }
