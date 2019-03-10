c05993c126ca8219e3bc8c1e5fc7a7d5fd3f1da4
spaceface777 <spaceface7777@gmail.com>
2019-03-10 17:50:02 +0100

    Fix login

--- web_client/js/login.js
+++ web_client/js/login.js
@@ -1,12 +1,18 @@
-if (getCookie('hermes_style') == 'dark'){
+if (getCookie('hermes_style') == 'dark') {
     $('#hermes_style').attr('href', 'css/dark/login.css');
 }
 
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "fast");
-});
+$(document).ready(() => {
 
-let cookie_notice = $("#cn");
-cookie_notice.click(()=>{
-    cookie_notice.fadeOut("fast");
-});
+    $('.message a').click(() => {
+        $('form').animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, "fast");
+    });
+
+    let cookie_notice = $("#cn");
+    cookie_notice.click(() => {
+        cookie_notice.fadeOut("fast");
+    });
+})
