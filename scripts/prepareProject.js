e25a207e3e770f4de46faa4475feb53132d521da
theperkinrex <theperkinrex@gmail.com>
2019-05-09 21:59:42 +0200

    Update prepare_project.js

--- scripts/prepareProject.js
+++ scripts/prepareProject.js
@@ -4,7 +4,7 @@ const TimeUUID = require('cassandra-driver').types.TimeUuid;
 const webPush = require('web-push');
 
 const configTemplate = {
-    generalToken: new TimeUUID(),
+    generalToken: (new TimeUUID()).toString(),
     mainIP: "localhost:8080",
     port: 8080,
     forceHAConnect: false,
@@ -12,45 +12,59 @@ const configTemplate = {
     db: {
         hosts: ['127.0.0.1:9042'], // Default localhost Cassandra URL
         username: 'cassandra', // Default Cassandra username & password
-        password: 'cassandra', 
+        password: 'cassandra',
         datacenter: 'datacenter1'
     }
 };
 
-function writeJSON(json = {}) {
-    let config = configTemplate;
-    for (key of Object.keys(config)) {
-        if (json[key]) {
-            config[key] = json[key]
+function checkObject(obj, template, name = 'object', fix=false, info = true, path = '') {
+    let correct = true;
+    let correctedObj = {...template}; // Copy the template
+    for (let key in template) {
+        if (typeof obj[key] !== typeof template[key]) {
+            if (info && !obj[key]){
+                console.log(`${path}${key} is missing from ${name}`);
+                if(fix) console.log(`Adding ${path}${key}`)
+            }
+            if (info && obj[key]){
+                console.log(`${path}${key} isn't the correct type set in ${name}. It should be ${typeof template[key]}, but it is ${typeof obj[key]}`);
+                if(fix) console.log(`Changing ${path}${key} from ${typeof obj[key] == 'string'?`"${obj[key]}"`:obj[key]} to ${typeof template[key] == 'string'?`"${template[key]}"`:template[key]}`)
+            }
+            
+            correct = false;
+        } else if ((typeof obj[key]) === 'object') {
+            robj = checkObject(obj[key], template[key], name, fix, info, `${path}${key}.`)
+            correct = robj.correct
+            if(fix){
+                correctedObj[key] = robj.correctedObj
+            }
+        }else if(fix){
+            correctedObj[key] = obj[key]
         }
     }
-    fs.writeFile('config.json', JSON.stringify(config, null, '\t'), err => {
-        if (err) throw err;
-        console.log('config.json created');
-    });
+    return {correct, correctedObj};
 }
 
 fileExists('config.json', (err, exists) => {
     if (err) throw err;
+    let config_o = {...configTemplate} // Cloning the object
+    let config_correct = false;
+    
     if (exists) {
         const config = require('../config.json');
-        let config_correct = true;
-        for (key of Object.keys(configTemplate)) {
-            if (config[key] == undefined) {
-                console.log(`${key} is missing from config.json`);
-                config_correct = false;
-                if(key == 'global_channel_uuid'){
-                    console.log('Please set the global channel UUID')
-                }
-            }
-        }
-        if (!config_correct) {
-            writeJSON(config);
-        } else {
+        let o = checkObject(config, configTemplate, 'config.json', true)
+        config_correct = o.correct
+        config_o = o.correctedObj
+        if (config_correct) {
             console.log('config.json is correct');
         }
     } else {
         console.log('config.json is misssing');
-        writeJSON();
+    }
+    if(!config_correct){
+        fs.writeFile('config.json', JSON.stringify(config_o, null, '\t'), err => {
+            if (err) throw err;
+            console.log(`config.json ${exists?'updated':'created'}`);
+        });
     }
 });
